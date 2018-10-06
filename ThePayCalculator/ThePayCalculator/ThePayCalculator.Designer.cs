9effce3f327be491be15dc254a04c0bef4831e2c
Roman Pelikh <pelikh.roman@gmail.com>
2018-10-06 18:35:29 -0300

    Add files via upload

---
+++ ThePayCalculator/ThePayCalculator/ThePayCalculator.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ThePayCalculator
+{
+    partial class ThePayCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblHourPayRate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.txtHourRate = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblOvertime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe Print", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblTitle.Location = new System.Drawing.Point(112, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(253, 60);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Pay Calculator";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoursWorked.Location = new System.Drawing.Point(59, 100);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(126, 17);
+            this.lblHoursWorked.TabIndex = 1;
+            this.lblHoursWorked.Text = "Hours Worked:";
+            // 
+            // lblHourPayRate
+            // 
+            this.lblHourPayRate.AutoSize = true;
+            this.lblHourPayRate.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHourPayRate.Location = new System.Drawing.Point(45, 132);
+            this.lblHourPayRate.Name = "lblHourPayRate";
+            this.lblHourPayRate.Size = new System.Drawing.Size(138, 17);
+            this.lblHourPayRate.TabIndex = 2;
+            this.lblHourPayRate.Text = "Hourly Pay Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Overtime Hours:";
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(191, 99);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(113, 20);
+            this.txtHoursWorked.TabIndex = 1;
+            // 
+            // txtHourRate
+            // 
+            this.txtHourRate.Location = new System.Drawing.Point(191, 129);
+            this.txtHourRate.Name = "txtHourRate";
+            this.txtHourRate.Size = new System.Drawing.Size(113, 20);
+            this.txtHourRate.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(321, 95);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(89, 25);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(321, 126);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(89, 25);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(321, 158);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(89, 25);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Image = global::ThePayCalculator.Properties.Resources.calc;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 306);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 100);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(48, 210);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(362, 93);
+            this.lblAnswer.TabIndex = 11;
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOvertime
+            // 
+            this.lblOvertime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOvertime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOvertime.Location = new System.Drawing.Point(191, 162);
+            this.lblOvertime.Name = "lblOvertime";
+            this.lblOvertime.Size = new System.Drawing.Size(113, 20);
+            this.lblOvertime.TabIndex = 3;
+            // 
+            // ThePayCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(455, 406);
+            this.Controls.Add(this.lblOvertime);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHourRate);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHourPayRate);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ThePayCalculator";
+            this.Text = "Play Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.Label lblHourPayRate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.TextBox txtHourRate;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblOvertime;
+    }
+}
+
---
+++ ThePayCalculator/ThePayCalculator/ThePayCalculator.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ThePayCalculator
+{
+    public partial class ThePayCalculator : Form
+    {
+        public ThePayCalculator()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach(Control txtBox in this.Controls)
+            {
+                if(txtBox is TextBox || txtBox is Label 
+                    && (txtBox.Name == "lblAnswer"  
+                    || txtBox.Name == "lblOvertime")){
+                    txtBox.Text = "";
+                    
+                }
+            }
+
+            txtHoursWorked.Focus();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            this.MinimizeBox = false;
+            this.MaximizeBox = false; 
+
+
+           
+
+
+
+            
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try {
+
+
+                string pattern = @"(\d+)";
+
+                Match txtHoursWorkedDigits = Regex.Match(txtHoursWorked.Text, pattern);
+                Match txtHoursRate = Regex.Match(txtHourRate.Text, pattern);
+
+               // if (Tryparse.)
+
+
+                if (txtHoursWorkedDigits.Success && txtHoursRate.Success)
+                {
+
+                    //Get input
+                    decimal hoursWorked = decimal.Parse(txtHoursWorked.Text);
+                    decimal payRate = decimal.Parse(txtHourRate.Text);
+                    
+                    //Create variables 
+                    decimal netPay;
+                    decimal grossPay;
+                    decimal decuctionTax = 0.15m;
+                    decimal hoursNormWorked = 40m;
+
+                    if (hoursWorked > 40)
+                    {
+
+
+                        decimal overtimeHours = hoursWorked - hoursNormWorked;
+                        decimal overtimePay = overtimeHours * (payRate * 1.5m);
+                        lblOvertime.Text = overtimeHours.ToString();
+
+
+                        decimal regularPay = hoursNormWorked * payRate;
+                        grossPay = overtimePay + regularPay;
+                        netPay = grossPay * (1 - decuctionTax);
+
+                        lblAnswer.Text = "Your Gross Pay is " + grossPay.ToString("c")
+                            + Environment.NewLine + "Your Net Pay is " + netPay.ToString("c");
+
+                        MessageBox.Show(lblAnswer.Text);
+                    }
+                    else if (hoursWorked <= 40)
+                    {
+
+                        grossPay = hoursWorked * payRate;
+                        netPay = grossPay * (1 - decuctionTax);
+
+                        lblAnswer.Text = "Your Gross Pay is " + grossPay.ToString("c")
+                            + Environment.NewLine + "Your Net Pay is " + netPay.ToString("c");
+
+                        MessageBox.Show(lblAnswer.Text);
+                    }
+
+
+                } else 
+                {
+                    MessageBox.Show("TEXTBOX ACCEPTS DIGITS ONLY");
+                    
+                }
+
+
+            } catch (Exception err)
+            {
+                MessageBox.Show("An error has occured: " + err.Message);
+            }
+            finally
+            {
+                txtHoursWorked.Focus();
+            }
+
+
+        }
+    }
+}
